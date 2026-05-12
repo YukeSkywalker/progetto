06ce2b1103f03e6855dc3c5b948a775be4206075
Luca <locricchioluca@itis-molinari.eu>
2026-05-12 22:12:09 +0200

    correzzione rotta principale '/' e formattazione server

--- server/server.js
+++ server/server.js
@@ -1,56 +1,69 @@
+//Impoto le librerie necessarie
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
 
+// Configurazione variabili ambiente per IP e PORT
 const IP = process.env.IP || "localhost";
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-//  Static frontend
+//  Configurazione FrontEnd (static files)
 app.use(express.static(path.join(__dirname, 'client')));
 
+// Rotta Principale 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+
+
+// Leva sto DB finto 
 let notes = [
-{ id: 1, text: "Prima notassssss" },
-{ id: 2, text: "Seconda nota e bastaaaaaasecondo" },
-{ id: 3, text: "terza" }
+    { id: 1, text: "Prima notassssss" },
+    { id: 2, text: "Seconda nota e bastaaaaaasecondo" },
+    { id: 3, text: "terza" }
 ];
 
 
+// -------- Rotte API ----------- 
+
 // GET -> ottiene tutte le note
 app.get("/notes", (req, res) => {
-res.json(notes);
+    res.json(notes);
 });
 
 
 // POST -> aggiunge una nota
 app.post("/notes", (req, res) => {
-const newNote = {
-id: notes.length + 1,
-text: req.body.text
-};
+    const newNote = {
+        id: notes.length + 1,
+        text: req.body.text
+    };
 
-notes.push(newNote);
+    notes.push(newNote);
 
-res.json(newNote);
+    res.json(newNote);
 });
 
 
 // DELETE -> elimina una nota
 app.delete("/notes/:id", (req, res) => {
 
-const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id);
 
-notes = notes.filter(note => note.id !== id);
+    notes = notes.filter(note => note.id !== id);
 
-res.json({
-message: "Nota eliminata"
-});
+    res.json({
+        message: "Nota eliminata"
+    });
 });
 
 
 app.listen(PORT, () => {
-console.log(`Server disponibile su http://${IP}:${PORT}.`);
+    console.log(`Server disponibile su http://${IP}:${PORT}.`);
 });
